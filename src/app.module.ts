f4632b27c1401670255ac3b3c0155fe6bf90abe8
Leandro Beloti Kornelius <leobelko@gmail.com>
2023-11-03 23:56:27 -0300

    feat: adding needed modules for the app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TaskModule } from './task/task.module';
+import { LabelModule } from './label/label.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TaskModule, LabelModule, PrismaModule],
 })
 export class AppModule {}
