5871b46652d0550299a5fb62c31d1b834c703da8
Leandro Beloti Kornelius <leobelko@gmail.com>
2023-11-05 23:05:04 -0300

    feat: made the crud for label

--- src/label/label.controller.ts
+++ src/label/label.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Param, Put, Delete } from '@nestjs/common';
 import { LabelService } from './label.service';
 import { LabelDTO } from './label.dto';
 
@@ -6,8 +6,33 @@ import { LabelDTO } from './label.dto';
 export class LabelController {
     constructor(private readonly labelService: LabelService) {}
 
+    // CREATE Route
     @Post()
     async create(@Body() data: LabelDTO) {
         return this.labelService.create(data);
+    }
+
+    // GET ALL Route
+    @Get()
+    async findLabels() {
+        return this.labelService.findLabels();
+    }
+
+    // GET ONE route 
+    @Get(":id") 
+    async findLabel(@Param("id") id: string) {
+        return this.labelService.findLabel(Number(id));
+    }
+
+    // UPDATE
+    @Put(":id")
+    async updateLabelText(@Param("id") id: string, text: string) {
+        return this.labelService.updateLabelText(Number(id), text);
+    }
+
+    // DELETE
+    @Delete(":id")
+    async deleteLabel(@Param("id") id: string) {
+        return this.labelService.deleteLabel(Number(id));
     }
 }
--- src/label/label.service.ts
+++ src/label/label.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { LabelDTO } from './label.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { error } from 'console';
 
 @Injectable()
 export class LabelService {
@@ -14,12 +13,62 @@ export class LabelService {
         const label = await this.prisma.lABEL.create({
             data, 
         });
-
         return label;
     }
 
-    // READ ONE
-    async
+    // READ ALL
+    async findLabels() {
+        return await this.prisma.lABEL.findMany();
+    }
 
+    // READ ONE
+    async findLabel(id: number) {
+        const labelExists = await this.prisma.lABEL.findUnique({
+            where: {
+                id,
+            }
+        });
+        if (!labelExists) {
+            throw new Error("Label does not exist");
+        } 
+        return labelExists;
+    }
+
+    // UPDATE 
+    async updateLabelText(id: number, text: string) {
+        const labelExists = await this.prisma.lABEL.findUnique({
+            where: {
+                id,
+            }
+        });
+        if (!labelExists) {
+            throw new Error("Label does not exist");
+        } 
+        return await this.prisma.lABEL.update({
+            where: {
+                id,
+            },
+            data: {
+                text,
+            }
+        });
+    }
+
+    // DELETE
+    async deleteLabel(id: number) {
+        const labelExists = await this.prisma.lABEL.findUnique({
+            where: {
+                id,
+            }
+        });
+        if (!labelExists) {
+            throw new Error("Label does not exist");
+        } 
+        return this.prisma.lABEL.delete({
+            where: {
+                id,
+            },
+        });
+    }
 
 }
