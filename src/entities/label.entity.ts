fbc9d77ff71422119b06205171c916bf94a819d7
Leandro Kornelius <leobelko@gmail.com>
2023-11-12 00:33:34 -0300

    fix: fixed not empty possibility

--- src/entities/label.entity.ts
+++ src/entities/label.entity.ts
@@ -1,10 +1,13 @@
 import { LABEL } from "@prisma/client";
-import { IsNumber, IsString } from "class-validator";
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
 
 export class LabelEntity implements LABEL {
     @IsNumber()
     id: number
 
+    @IsNotEmpty({
+        message: "O atributo deve existir"
+    })
     @IsString({
         message: "O atributo text precisa ser string"
     })
--- src/entities/task.entity.ts
+++ src/entities/task.entity.ts
@@ -1,15 +1,21 @@
 import { TASK } from "@prisma/client";
-import { IsBoolean, IsNumber, IsOptional, IsString } from "class-validator";
+import { IsBoolean, IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
 
 export class TaskEntity implements TASK {
     @IsNumber()
     id: number
 
+    @IsNotEmpty({
+        message: "O atributo deve existir"
+    })
     @IsString({
         message: "O atributo text precisa ser string"
     })
     text: string
 
+    @IsNotEmpty({
+        message: "O atributo deve existir"
+    })
     @IsBoolean({
         message: "O atributo checked precisa ser boolean"
     })
